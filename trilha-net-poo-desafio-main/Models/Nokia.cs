81b01fb5e060403ffb5feafbf1e869357d31a958
Gabriel Cordeiro <144538771+Kiwa98@users.noreply.github.com>
2024-01-25 14:42:29 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Models/Nokia.cs
@@ -0,0 +1,17 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero) : base(numero)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativos no Nokia {nomeApp}");
+        }
+
+    }
+}
---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia1 = new Nokia("1545412");
+Iphone iphone1 = new Iphone("8465155");
+
+nokia1.InstalarAplicativo("WhatsApp");
+
+iphone1.InstalarAplicativo("Instagram");
+
+nokia1.Ligar();
+
+nokia1.ReceberLigacao();
+
+iphone1.ReceberLigacao();
+iphone1.Ligar();
